77842a761373bd87af456a0f5510ec6825348f98
Miwa <paintfx@gmail.com>
2018-02-15 18:54:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    current_time = Time.now.hour
 
-    if current_time >= 12
+    if Time.now.hour >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
